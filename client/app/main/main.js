76fc7002c7f7593f844f0192aa29267c37c3d472
martin butler <martin.fullstack@gmail.com>
2015-01-27 14:38:38 -0500

    init

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('imdbProjectApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
