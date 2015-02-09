486f4546e21a1bc0dc96e8349335b4ce0da04690
martin butler <martinebutler@gmail.com>
2015-02-08 20:19:12 -0500

    create rating import util. parses date from ratings.list and generates a json file for mongoimport

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,6 +18,7 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/rating/rating.socket').register(socket);
   require('../api/director/director.socket').register(socket);
   require('../api/actor/actor.socket').register(socket);
   require('../api/actress/actress.socket').register(socket);
