dacface144826d3248750736bdb8ea9916a3e7a7
martin butler <martinebutler@gmail.com>
2015-03-14 22:34:48 -0400

    set mongoose schema.

--- server/api/akaTitle/akaTitle.model.js
+++ server/api/akaTitle/akaTitle.model.js
@@ -5,8 +5,8 @@ var mongoose = require('mongoose'),
 
 var AkaTitleSchema = new Schema({
   name: String,
-  info: String,
-  active: Boolean
+  title: String,
+  attributes: String
 });
 
 module.exports = mongoose.model('AkaTitle', AkaTitleSchema);
