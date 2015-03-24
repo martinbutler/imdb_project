0cb73bdf14204894856024f49d27d4c6c190e611
martin butler <martinebutler@gmail.com>
2015-03-24 14:34:33 -0400

    update mongoose schema. _id reset to default. udpate logic to create a record for each director title pairing.

--- server/api/utils/import/import_directors.js
+++ server/api/utils/import/import_directors.js
@@ -38,68 +38,59 @@ rl.on('line', function(line) {
         fs.appendFileSync(outputFile, JSON.stringify(newRecord) + "\n");
       } else {
         var parseArray = line.split('\t');
-        // check if line is includes director's name
+        // check if line includes director's name
         if (parseArray[0] !== '') {
-          // check if previous record has been written
-          if (newRecord._id) {
-            fs.appendFileSync(outputFile, JSON.stringify(newRecord) + "\n");
-            // prep for new record
-            newRecord = {};
-            newRecord._id = parseArray[0];
-            newRecord.titles = [];
-          } else {
-            newRecord._id = parseArray[0];
-            newRecord.titles = [];
-          }
+          newRecord.name = parseArray[0];
         }
         var fullTitleData = parseArray[parseArray.length-1];
-        var title, addInfo;
         // parse the titles marked as suspended
         if(suspendMatches = fullTitleData.match(regExpSuspended)) {
-          title = fullTitleData.substring(0, fullTitleData.indexOf(suspendMatches[0]) + suspendMatches[0].length).replace(/"/g, '\\"');
+          newRecord.title = fullTitleData.substring(0, fullTitleData.indexOf(suspendMatches[0]) + suspendMatches[0].length).replace(/"/g, '\\"');
         }
         // parses the titles with TV or V tags
-        if (!title) {
+        if (!newRecord.title) {
           var parMatches = fullTitleData.match(regExpParentheses);
           var i = 1;
           var parMatchLen = parMatches.length;
           while (i < parMatchLen){
             if (parMatches[i] === "(TV)" || parMatches[i] === "(V)" || parMatches[i] === "(VG)"){
-              title = fullTitleData.substring(0, fullTitleData.indexOf(parMatches[i]) + parMatches[i].length).replace(/"/g, '\\"');
+              newRecord.title = fullTitleData.substring(0, fullTitleData.indexOf(parMatches[i]) + parMatches[i].length).replace(/"/g, '\\"');
               i = parMatchLen;
             }
             i++;
           }
         }
         // parse the titles with episode information in the record
-        if (!title) {
+        if (!newRecord.title) {
           if (episodeMatch = fullTitleData.match(regExpEpisode)){
-            title = fullTitleData.substring(0, fullTitleData.indexOf(episodeMatch[0])+ episodeMatch[0].length).replace(/"/g, '\\"');
+            newRecord.title = fullTitleData.substring(0, fullTitleData.indexOf(episodeMatch[0])+ episodeMatch[0].length).replace(/"/g, '\\"');
           }
         }
         // Parse title addressing multi-parentheses find
         // based on expeted year formats in data
         // Based on data dump, parMatches assignment will have
         // executed if title has not be defined
-        if (!title) {
+        if (!newRecord.title) {
           for (var i = 0; i < parMatchLen; i++) {
             if (parMatches[i] === '(????)' || parMatches[i].substring(0, 6) === '(????/' || parseInt(parMatches[i].substring(1)) >= 1870) {
-              title = fullTitleData.substring(0, fullTitleData.indexOf(parMatches[i])  + parMatches[i].length).replace(/"/g, '\\"');
+              newRecord.title = fullTitleData.substring(0, fullTitleData.indexOf(parMatches[i])  + parMatches[i].length).replace(/"/g, '\\"');
             }
           }
         }
         // parse titles with no previous matching criteria, including
         // episodes, TV tag, V tag, VG tag, and year formats
         // if no other match for title is found, use the full title data
-        if (!title) {
-          title = fullTitleData.replace(/"/g, '\\"');
+        if (!newRecord.title) {
+          newRecord.title = fullTitleData.replace(/"/g, '\\"');
+          console.log(fullTitleData)
         }
-        // parse addInfo, if it exists
-        if (addInfoMatch = fullTitleData.substring(title.length).trim()) {
-            addInfo = fullTitleData.substring(title.length).trim().replace(/"/g, '\\"');
+        // parse additionalInfo, if it exists
+        if (additionalInfoMatch = fullTitleData.substring(newRecord.title.length).trim()) {
+            newRecord.additionalInfo = fullTitleData.substring(newRecord.title.length).trim().replace(/"/g, '\\"');
         }
-        // save title and associated data in director's title array
-        newRecord.titles.push({"title": title, "addInfo": addInfo});
+        fs.appendFileSync(outputFile, JSON.stringify(newRecord) + "\n");
+        delete newRecord.title;
+        delete newRecord.additionalInfo;
       }
     }
   // logic to skip non movie data at the head of the file
